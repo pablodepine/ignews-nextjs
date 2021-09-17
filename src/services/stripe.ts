b128c6e14a21568651d338fdd77d87ffce1b7d5d
pablodepine <pablodepine96@gmail.com>
2021-09-16 22:10:54 -0300

    Update commit

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,10 +1,8 @@
 import Stripe from 'stripe'
 import { version } from '../../package.json'
 
-export const STRIPE_API_KEY = 'sk_test_51IkRwRKmRdPeIMh0F3zDzMjkkGn8BPVcV6s6cHQvY4vQKpt5N0lU2hp4dZy2lJzveLC90bWOkQwcpSZoJL6KGQ1f00ZMbtTIxR';
-
 export const stripe = new Stripe(  
-  STRIPE_API_KEY,
+  process.env.STRIPE_API_KEY,
   {
     apiVersion: '2020-08-27',
     appInfo: {
